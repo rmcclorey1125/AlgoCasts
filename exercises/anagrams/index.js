fa6331cf22b97be45f79f4650ea4bd546e33346f
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-10-21 16:15:26 -0400

    section 11

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let strA = {}
+    let strB = {}
+
+    let lettersA = stringA.replace(/[^\w]/g, "").toLowerCase()
+    let lettersB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+    for(let char of lettersA){
+        if(strA[char]){
+            strA[char]++
+        }else{
+            strA[char] = 1
+        }
+    }
+
+    for(let char of lettersB){
+        if(strB[char]){
+            strB[char]++
+        }else{
+            strB[char] = 1
+        }
+    }
+
+    if(Object.keys(strA).length !== Object.keys(strB).length){
+        return false
+    }
+
+    for(let char in strA){
+        if(strA[char] !== strB[char]){
+            return false
+        }
+    }
+
+    return true
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//     return cleanString(stringA) === cleanString(stringB)
+// }
+
+// function cleanString(str) {
+//     return str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('')
+// }
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,18 +9,27 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    chunked = []
+    let chunked = []
+    let index = 0
 
-    for(let element of array){
-        const last = chunked[chunked.length - 1]
-
-        if(!last || last.length === size){
-            chunked.push([element])
-        } else {
-            last.push(element)
-        }
+    while(index < array.length){
+        chunked.push(array.slice(index, index + size))
+        index = index + size
     }
     return chunked
 }
 
 module.exports = chunk;
+
+// chunked = []
+
+// for(let element of array){
+//     const last = chunked[chunked.length - 1]
+
+//     if(!last || last.length === size){
+//         chunked.push([element])
+//     } else {
+//         last.push(element)
+//     }
+// }
+// return chunked
